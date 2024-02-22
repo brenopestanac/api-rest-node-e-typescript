a0298f9f810e238bea05015742c6af74af852c09
brenopestanac <brenopestana28@gmail.com>
2024-02-22 11:55:46 -0300

    feat: Estruturação de pastas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,15 +3,20 @@ import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
-router.post('/teste', (req, res) => {
-  console.log(req);
-
-  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
 
 router.get('/', (req, res) => {
   return res.send('Olá, Dev!');
 });
+
+
+router.post(
+  '/teste',
+  (req, res) => {
+  // console.log(req);
+
+    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+  },
+);
 
 
 export { router };
